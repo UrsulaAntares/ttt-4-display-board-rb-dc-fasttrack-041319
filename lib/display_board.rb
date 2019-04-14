7ff3d5f9498b7dcf14e4335b261b34b93996c875
Ursula Miller <ursula.miller+fi@gmail.com>
2019-04-14 11:57:57 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board(board)
+  line = "-----------"
+  puts board[0] + | + board[1] + | board[2]
+  puts line
+  puts "   |   |   "
+  puts line
+  puts "   |   |   "
+end
+  
